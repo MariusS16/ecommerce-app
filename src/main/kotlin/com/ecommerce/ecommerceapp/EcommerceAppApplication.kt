97512df90a1a11432747663dc72108bc6d0bfb34
MariusS16 <slincumarius0716@gmail.com>
2025-12-28 19:33:15 +0200

    Initial commit

---
+++ src/main/kotlin/com/ecommerce/ecommerceapp/EcommerceAppApplication.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerceapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceAppApplication>(*args)
+}
